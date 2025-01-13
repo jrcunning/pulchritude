4c8638efcd21f6d84e9d93c8c5557b7c29d6f8ad
TrinityConn <trinity.conn@ssh3.hac.uri.edu>
2025-01-13 18:06:27 -0500

    edit trim script

--- scripts/bbduk_trim.sh
+++ scripts/bbduk_trim.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 #SBATCH --job-name=bbduktrim
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=20
 #SBATCH --mem=50gb
 #SBATCH --time=10:00:00
 #SBATCH --output=trim.out
 #SBATCH --output=trim.error
 
 source ~/.bashrc
-conda  activate bbmap_trim
+conda  activate bbmap
+module load parallel/20220722-GCCcore-11.3.0 
+cd  /data/putnamlab/tconn/apul_reseq
 
-cd  /data/putnamlab/tconn/reseq
+mkdir trimmed 
 
-for i in 
+DIR=/data/putnamlab/tconn/apul_reseq/H2WLFDSXF/8740
+OUT=/data/putnamlab/tconn/apul_reseq/trimmed
 
-
-bbduk.sh \
-in1=/storage/group/dut374/default/trinity/rawdata/Unknown_BK461-020033_1.fq.gz \
-in2=/storage/group/dut374/default/trinity/rawdata/Unknown_BK461-020033_2.fq.gz \
-out1=~/scratch/20033_1trimmed.fq.gz \
-out2=~/scratch/20033_2trimmed.fq.gz \
-ref=/storage/group/ibb3/default/tools/bbmap/resources/adapters.fa \
+parallel --plus echo {%R._001.fastq.gz} ::: *.fastq.gz | parallel 'bbduk.sh \
+in1=$DIR/.fq.gz \
+in2=$DIR/Unknown_BK461-020033_2.fq.gz \
+out1=$OUT/20033_1trimmed.fq.gz \
+out2=$OUT/20033_2trimmed.fq.gz \
+ref=/data/putnamlab/tconn/apul_reseq/adapters.fa \
 ktrim=r \
 k=23 \
 mink=11 \
