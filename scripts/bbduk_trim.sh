884163aca2c086904a66e9c1423926804bdd3088
TrinityConn <trinity.conn@ssh3.hac.uri.edu>
2025-01-14 14:27:39 -0500

    finally got it working

--- scripts/bbduk_trim.sh
+++ scripts/bbduk_trim.sh
@@ -4,22 +4,21 @@
 #SBATCH --ntasks=20
 #SBATCH --mem=50gb
 #SBATCH --time=10:00:00
-#SBATCH --output=trim.out
-#SBATCH --output=trim.error
+#SBATCH --account=putnamlab
+#SBATCH -o slurm-%j.out
+#SBATCH -e slurm-%j.error
 
 source ~/.bashrc
 conda  activate bbmap
 module load parallel/20220722-GCCcore-11.3.0 
-cd  /data/putnamlab/tconn/apul_reseq/H2WLFDSXF/8740
 
-DIR=/data/putnamlab/tconn/apul_reseq/H2WLFDSXF/8740
-OUT=/data/putnamlab/tconn/apul_reseq/trimmed
+cd  /data/putnamlab/tconn/apul_reseq/H2WLFDSXF/8740
 
 parallel -j 20 bbduk.sh \
 in1={} \
 in2={=s/R1/R2/=} \
-out1=$OUT/{%.R1.fastq.gz}.trimmed.R1.fastq.gz \
-out2=$OUT/{%.R1.fastq.gz}.trimmed.R2.fastq.gz \
+out1=/data/putnamlab/tconn/apul_reseq/trimmed/{}.trimmed.R1.fastq.gz \
+out2=/data/putnamlab/tconn/apul_reseq/trimmed/{}.trimmed.R2.fastq.gz \
 ref=/home/trinity.conn/.conda/envs/bbmap/opt/bbmap-39.13-1/resources/adapters.fa \
 ktrim=r \
 k=23 \
